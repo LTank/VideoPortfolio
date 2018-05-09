60f0ecca0e5b074d79eaa6978013afe4599430e1
Jonas Ladefoged Holm <jonas@holm.tech>
2018-05-09 16:37:00 +0200

    First fade effect works! Not finished tho!

--- src/main/resources/static/javascript/genreSortScript.js
+++ src/main/resources/static/javascript/genreSortScript.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 
     $("#all").click(function () {
-        $("img").toggleClass("grey");
+        $(".video-link img").fadeTo("slow", 1);
     });
 
-}
+    $("#short_film").click(function () {
+        $(".video-link img").not(".short_film img").fadeTo("slow", 0.2);
+    });
+
+});
